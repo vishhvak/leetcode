fa7b54ec1ac7f417869786faf0870fbd98a13038
Vishhvak <vishhvak@outlook.com>
2022-10-17 23:11:15 -0400

    Time: 87 ms (54.69%), Space: 14.8 MB (30.86%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,5 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        # Method 1, join string with alpha numeric characters and lowercase
         t = ''.join(filter(str.isalnum, s)).lower()
-        #print(t)
+        # Check if string is equal to reverse
         return t == t[::-1]
+    
+        
